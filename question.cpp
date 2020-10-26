854e006495c9bebf3c8ae7d208939d7d2156469d
Max Jaycee <72267910+MaxJaycee@users.noreply.github.com>
2020-10-26 20:20:00 +0530

    Create question.cpp

---
+++ question.cpp
@@ -0,0 +1,2 @@
+//A salesman has n things to sale. The cost price of all n things is different out of which p things he is selling on m% profit and n-p things he is going to sell on x% loss. Find his net profit or loss.
+#include<iostream>
