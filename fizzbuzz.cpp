136251fe404244d6988e4f2a573bc04ebdfa1638
Max Jaycee <72267910+MaxJaycee@users.noreply.github.com>
2020-10-26 20:12:45 +0530

    Add files via upload

---
+++ fizzbuzz.cpp
@@ -0,0 +1,40 @@
+/*
+Problem : a) print "FizzBuzz" ,if N (user input) is divisile to 3 and 5,
+		  b) print "Fizz" ,if N (user input) is divisile to 3 , 
+		  c) print "Buzz" ,if N (user input) is divisile to 5
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int i, n ;
+	cout<<"Enter a number till which you want the output: ";
+	cin>> n ;
+	for (i = 1; i <= n; i++)
+	{
+		if(i%3 ==0 && i%5==0)
+		{
+			cout << "FizzBuzz, ";
+		}
+		else if (i % 3 == 0)
+		{
+			cout << "Fizz, ";
+		}
+		else if (i % 5 == 0)
+		{
+			cout << "Buzz, ";
+		}
+		else
+		{
+			cout << i << ", ";
+		}
+		if (i % 10 == 0)
+		{
+			cout << "\n";
+		}
+	}
+	return 0;
+} 
